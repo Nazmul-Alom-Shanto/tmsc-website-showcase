f50073a6e04c3cbc90e8f47f91fea6eddd7f8ba2
Nazmul Alom Shanto <nazmulalomshanto2006@gmail.com>
2025-09-06 22:51:06 +0600

    add: another alumni added

---
+++ assets/images/alumni/Profile.js
@@ -0,0 +1,93 @@
+const alumniData = [
+  {
+    name: "Abir Hossen Abdullah",
+    title: "CSE, BUET",
+    details: "Ex-President",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Abir%20Hossen%20Abdullah.jpg` ,
+    socialLinks: [{
+        name: "Facebook",
+        url: "https://www.facebook.com/ahabdullahofficial",
+        icon: <Facebook />,
+      }],
+  },
+  {
+    name: "Tahsin Ahmad",
+    title: "CSE, BUET",
+    details: "Ex-Coordinator of Fest",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Tahsin%20Ahmad.jpg` ,
+    socialLinks: [],
+  },
+  {
+    name: "Eyakub Islam Shahed",
+    title: "Satkhira Medical College",
+    details: "Ex-President",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Eyakub%20Islam%20Shahed.jpg` ,
+    socialLinks: [{
+        name: "Facebook",
+        url: "https://www.facebook.com/sahed.sahed.3745",
+        icon: <Facebook />,
+      }],
+  },
+  
+  {
+    name: "Shah Mostofa Reza",
+    title: "EEE, BUET",
+    details: "Ex-President",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Shah%20Mostofa%20Reza.jpg` ,
+    socialLinks: [ {
+        name: "Facebook",
+        url: "https://www.facebook.com/shahmostafareza",
+        icon: <Facebook />,
+      }],
+  },
+   {
+    name: "Atfan Bin Nur",
+    title: "CSE, CUET",
+    details: "Ex-Member, TMSC",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Atfan%20Bin%20Nur.jpg` ,
+    socialLinks: [ {
+        name: "Facebook",
+        url: "https://www.facebook.com/atfan.binnoor",
+        icon: <Facebook />,
+      }],
+  },
+  {
+    name: "Dr. Zinan Bin Nur",
+    title: "Armed Forces Medical College",
+    details: "Ex-Mentor, TMSC",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Dr.%20Zinan%20Bin%20Nur.jpg` ,
+    socialLinks: [{
+        name: "Facebook",
+        url: "https://www.facebook.com/zinanbin.nur",
+        icon: <Facebook />,
+      }],
+  },
+  {
+    name: "Adnan Hamjah",
+    title: "Mugda Medical College",
+    details: "Ex-Quiz Secretary, TMSC",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/Adnan%20Hamjah.jpg` ,
+    socialLinks: [  {
+        name: "Facebook",
+        url: "https://www.facebook.com/hamza.adnan.75098364",
+        icon: <Facebook />,
+      }],
+  },
+   {
+    /**
+     * MD. Shah Paran
+Ex Member
+Best Science Student-2020
+Awarded by TMSC
+     */
+    name: "MD. Shah Paran",
+    title: "Best Science Student-2022 (Awarded by TMSC)",
+    details: "Ex-Member, TMSC",
+    imageUrl: `${assetsBaseUrl}/assets/images/alumni/MD.%20Shah%20Paran.jpg` ,
+    socialLinks: [{
+        name: "Facebook",
+        url: "https://www.facebook.com/moham.mada.saha.parana.900843",
+        icon: <Facebook />,
+      }],
+  },
+];
